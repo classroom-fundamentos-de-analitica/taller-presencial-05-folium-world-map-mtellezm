482dad7f2ed044a35ab7a30a1d75df72f7350e9d
mariana <mtellezm@unal.edu.co>
2024-03-04 09:07:47 -0500

    funcion 3

--- country_scientific_prodcution.py
+++ country_scientific_prodcution.py
@@ -4,14 +4,14 @@
 import pandas as pd
 
 def load_affiliations():
+    """Carga el archivo scopus-papers.csv y retorna un dataframe con la columna 'Affiliations'"""
     dataframe = pd.read_csv(
-        "https://raw.githubusercontent.com/jdvelasq/datalabs/master/datasets/scopus-papers.csv",
-        sep=",", 
+        "scopus-papers.csv",
+        sep=",",
         index_col=None,
     )[["Affiliations"]]
     return dataframe
-    
-
+  
 
 def remove_na_rows(affiliations):
     """Elimina las filas con valores nulos en la columna 'Affiliations'"""
@@ -22,7 +22,29 @@ def remove_na_rows(affiliations):
     return affiliations
   
 
+def add_countries_column(affiliations):
+    """Transforma la columna 'Affiliations' a una lista de paises."""
+
+    affiliations = affiliations.copy()
+    affiliations["countries"] = affiliations["Affiliations"].copy()
+    affiliations["countries"] = affiliations["countries"].str.split(";")
+    # affiliations["countries"] = affiliations["countries"].map(
+    #     lambda x: [y.split(",") for y in x]
+    # )
+    # affiliations["countries"] = affiliations["countries"].map(
+    #     lambda x: [y[-1].strip() for y in x]
+    # )
+    # affiliations["countries"] = affiliations["countries"].map(set)
+    # affiliations["countries"] = affiliations["countries"].str.join(", ")
+
+    return affiliations
+  
+
+
 df = load_affiliations()
 df = remove_na_rows(df)
-print(df.sort_values("Affiliations").head())
-print(df.sort_values("Affiliations".tail()))
+df = add_countries_column(df)
+
+for i in range(5):
+    print("----")
+    print(df.Affiliations.values[i])
